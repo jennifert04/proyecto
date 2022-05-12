5314e6ccc72388cc74b1d375368601676665c68b
jennifert04 <102638386+jennifert04@users.noreply.github.com>
2022-05-12 16:39:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
-# Add to this file for the sample app lab
-from flask import Flask
-from flask import request
-from flask import render_template
+#llamar librerias
+from dash import Dash, html, dcc #libreria para servidor web de dara science(dash manipular servidores web con phyton, html emular 
+#configurar objetos html, dcc visualizar cosas bonitas )
+import pandas as pd             #libreria para procesar datos
+import plotly.graph_objs as go  #libreria para graficar 
+
+#declarar objetos principales
+app = Dash(__name__) #creé un objeto dash
+
+#cargar la base de datos
+df = pd.read_excel('data.xlsx')
 
 
-sample = Flask(__name__)
+#configurar el sitio web
+app.layout = html.Div([html.H1('Programa para el covid en AMVA'),
+                        html.Div('Información de la pagina'),
+                        dcc.Graph(id='map',figure={
+                            'data':[{'lat':df['Latitud'],
+                                    'lon':df['Longitud'],
+                                    'marker':{
+                                        'color': 150,
+                                        'size': df['Enfermos'],
+                                        'opacity': 0.6
+                                    },
+                                    'customdata': df['Enfermos'],
+                                    'type':'scattermapbox'}],
+                           'layout':{'mapbox':{
+                           'accesstoken': 'pk.eyJ1IjoibGVvbmFyZG9iZXRhbmN1ciIsImEiOiJjazlybGNiZWcwYjZ6M2dwNGY4MmY2eGpwIn0.EJjpR4klZpOHSfdm7Tsfkw',
+                           'center':{'lat':6.240737,'lon':-75.589900},
+                           'zoom':10},
+                           'hovermode':'closest',
+                           'margin':{'l':0,'r':0,'b':0,'t':0}} 
+                        })])
 
-@sample.route("/")
-def main():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    sample.run(host="0.0.0.0", port=5050)
+#funcion principal
+if __name__ == '__main__':
+    #cargar el objeto principal a todas las interfaces de red en el puerto 80
+    app.run_server(host='0.0.0.0',port=80) #servidor en memoria ram que se pegó a ese puerto
